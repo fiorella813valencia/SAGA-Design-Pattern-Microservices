6f2759eae9a3ff411a6c12b30440f9ab06837f81
fiorella813valencia <u20201b944@upc.edu.pe>
2024-02-25 11:11:14 -0500

    delivery microservice

---
+++ delivery-ms/src/main/java/com/delivery/deliveryms/entity/DeliveryRepository.java
@@ -0,0 +1,7 @@
+package com.delivery.deliveryms.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeliveryRepository extends CrudRepository<Delivery, Long> {
+
+}
